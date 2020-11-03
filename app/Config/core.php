0e1b35666f122811a8e39fc1b14d280be480fcaa
Zinzin92 <kevin-descours@outlook.fr>
2020-11-03 21:36:08 +0100

    initial

--- app/Config/core.php
+++ app/Config/core.php
@@ -362,7 +362,7 @@ if (Configure::read('debug') > 0) {
 }
 
 // Prefix each application on the same server with a different string, to avoid Memcache and APC conflicts.
-$prefix = 'mineweb_';
+$prefix = 'fivem_cms_';
 
 /**
  * Configure the cache used for general framework caching. Path information,
